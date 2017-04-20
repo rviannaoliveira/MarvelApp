f93406a173bd176294a9ed470403740b04d40974
Rodrigo Vianna <rodrigo.vianna.oliveira@gmail.com>
2017-04-19 23:23:42 -0300

    Add constant tag fragment

---
+++ app/src/main/java/com/rviannaoliveira/marvelapp/main/MarvelFragmentsConstant.kt
@@ -0,0 +1,10 @@
+package com.rviannaoliveira.marvelapp.main
+
+/**
+ * Criado por rodrigo on 19/04/17.
+ */
+object MarvelFragmentsConstant {
+    val COMICS = "COMICS"
+    val CHARACTERS = "CHARACTERS"
+    val FAVORITES = "FAVORITES"
+}
